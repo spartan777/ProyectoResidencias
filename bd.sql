4811306ceb6d7f1e65c0571ecc453c10834ece56
spartan777 <ing.javiercastrolagunes@gmail.com>
2016-11-13 21:22:51 -0600

    Tabla salones

--- bd.sql
+++ bd.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.1
+-- version 4.1.14
 -- http://www.phpmyadmin.net
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 11-11-2016 a las 19:33:48
--- Versión del servidor: 10.1.13-MariaDB
--- Versión de PHP: 5.6.20
+-- Tiempo de generación: 14-11-2016 a las 04:22:08
+-- Versión del servidor: 5.6.17
+-- Versión de PHP: 5.5.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -20,10 +20,11 @@ SET time_zone = "+00:00";
 -- Estructura de tabla para la tabla `carreras`
 --
 
-CREATE TABLE `carreras` (
+CREATE TABLE IF NOT EXISTS `carreras` (
   `id_carrera` varchar(13) NOT NULL,
   `nombre_carrera` varchar(50) NOT NULL,
-  `img_carrera` varchar(50) NOT NULL
+  `img_carrera` varchar(50) NOT NULL,
+  PRIMARY KEY (`id_carrera`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -40,13 +41,16 @@ INSERT INTO `carreras` (`id_carrera`, `nombre_carrera`, `img_carrera`) VALUES
 -- Estructura de tabla para la tabla `catedratico`
 --
 
-CREATE TABLE `catedratico` (
+CREATE TABLE IF NOT EXISTS `catedratico` (
   `id_catedratico` varchar(20) NOT NULL,
   `nombre` varchar(50) NOT NULL,
   `ape_materno` varchar(50) NOT NULL,
   `ape_paterno` varchar(40) NOT NULL,
   `correo` varchar(100) NOT NULL,
-  `id_carrera` varchar(13) NOT NULL
+  `id_carrera` varchar(13) NOT NULL,
+  PRIMARY KEY (`id_catedratico`),
+  KEY `id_catedratico` (`id_catedratico`),
+  KEY `catedratico_ibfk_1` (`id_carrera`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -55,13 +59,15 @@ CREATE TABLE `catedratico` (
 -- Estructura de tabla para la tabla `jefe_carrera`
 --
 
-CREATE TABLE `jefe_carrera` (
+CREATE TABLE IF NOT EXISTS `jefe_carrera` (
   `id_usuario` varchar(20) NOT NULL,
   `nombre` varchar(50) NOT NULL,
   `ape_materno` varchar(50) NOT NULL,
   `ape_paterno` varchar(40) NOT NULL,
   `correo` varchar(100) NOT NULL,
-  `id_carrera` varchar(13) NOT NULL
+  `id_carrera` varchar(13) NOT NULL,
+  PRIMARY KEY (`id_usuario`),
+  KEY `id_carrera` (`id_carrera`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -74,15 +80,35 @@ INSERT INTO `jefe_carrera` (`id_usuario`, `nombre`, `ape_materno`, `ape_paterno`
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `salones`
+--
+
+CREATE TABLE IF NOT EXISTS `salones` (
+  `id_salon` varchar(10) NOT NULL,
+  `nombre` varchar(30) NOT NULL,
+  PRIMARY KEY (`id_salon`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `salones`
+--
+
+INSERT INTO `salones` (`id_salon`, `nombre`) VALUES
+('21', 'Edit prueba');
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `usuario`
 --
 
-CREATE TABLE `usuario` (
-  `id_usuario` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `usuario` (
+  `id_usuario` int(11) NOT NULL AUTO_INCREMENT,
   `nombre_usuario` varchar(20) NOT NULL,
   `pass_usuario` varchar(50) NOT NULL,
-  `tipo_usuario` varchar(15) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `tipo_usuario` varchar(15) NOT NULL,
+  PRIMARY KEY (`id_usuario`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
 
 --
 -- Volcado de datos para la tabla `usuario`
@@ -96,46 +122,6 @@ INSERT INTO `usuario` (`id_usuario`, `nombre_usuario`, `pass_usuario`, `tipo_usu
 (5, '', '', ''),
 (6, '', '', '');
 
---
--- Índices para tablas volcadas
---
-
---
--- Indices de la tabla `carreras`
---
-ALTER TABLE `carreras`
-  ADD PRIMARY KEY (`id_carrera`);
-
---
--- Indices de la tabla `catedratico`
---
-ALTER TABLE `catedratico`
-  ADD PRIMARY KEY (`id_catedratico`),
-  ADD KEY `id_catedratico` (`id_catedratico`),
-  ADD KEY `catedratico_ibfk_1` (`id_carrera`);
-
---
--- Indices de la tabla `jefe_carrera`
---
-ALTER TABLE `jefe_carrera`
-  ADD PRIMARY KEY (`id_usuario`),
-  ADD KEY `id_carrera` (`id_carrera`);
-
---
--- Indices de la tabla `usuario`
---
-ALTER TABLE `usuario`
-  ADD PRIMARY KEY (`id_usuario`);
-
---
--- AUTO_INCREMENT de las tablas volcadas
---
-
---
--- AUTO_INCREMENT de la tabla `usuario`
---
-ALTER TABLE `usuario`
-  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 --
 -- Restricciones para tablas volcadas
 --
